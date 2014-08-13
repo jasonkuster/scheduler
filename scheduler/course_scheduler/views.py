ad1cc5dbf2cf725c91f9eade10c77d39882e5953
Stuart Long <stuart.richard.long@gmail.com>
2014-08-12 22:14:23 -0700

    working on jquery results

--- scheduler/course_scheduler/views.py
+++ scheduler/course_scheduler/views.py
@@ -186,6 +186,49 @@ def return_test(request):
 ##    #logging.debug('RETURNING')
 ##    return response
 
+def search(request)
+    #logging.debug('GOT A REQUEST')
+    toSend = {}
+    if request.method == 'GET'
+        #logging.debug('IT IS A GET')
+        #criterion = request.GET.get('Search', None)
+        #TODO better regexes
+        #patt = re.compile('(wwww ((www)(wwww)))(wwwwwww)')
+##       patt = re.compile('(wwww( )(d+(d+w)))')
+        criterion = request.GET['criterion']
+##        if patt.match(criterion)
+##            str = string.replace(criterion, ' ', '')
+##            arr = [None]2
+##            arr[0] = str[03]
+##            arr[1] = str[4]
+##            #arr = criterion.split(' ')
+##            classes = Instructs.objects.filter(meeting__meeting_class__dept__icontains=arr[0], meeting__meeting_class__class_number__icontains=arr[1])
+##        else
+
+        response = render(request, 'add.html', {'classes' : toSend, 'id' : id, 'form' : SearchForm()})
+        classes = Instructs.objects.filter(Q(meeting__meeting_class__classname__icontains=criterion) |
+                                           Q(meeting__meeting_class__dept__icontains=criterion) |
+                                           Q(meeting__meeting_class__class_number__icontains=criterion) |
+                                           Q(meeting__meeting_class__instructor__icontains=criterion))
+
+        for c in classes
+            toSend[c.pk] = False
+##            if Enrollment.objects.filter(student_id=id, event_id=c.meeting.id).exists()
+##                toSend[c] = True
+##            else
+##                toSend[c] = False
+    else
+        return None
+
+    response_data = {}
+    response_data['courses'] = classes
+    response_data['in_schedule'] = toSend
+    response_data['criterion'] = criterion
+    response = HttpResponse(json.dumps(response_data), content_type="application/json")
+   ## response = render(request, 'search_result.html', {'classes'  toSend})
+    #logging.debug('RETURNING')
+    return response
+
 #   The info view is called whenever
 #   the user clicks on a meeting-time's
 #   name. The function is passed a
