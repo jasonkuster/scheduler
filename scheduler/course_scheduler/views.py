6dc1f86b7959cee645b84f896faef754d787f1c9
Stuart Long <stuart.richard.long@gmail.com>
2014-08-12 21:45:59 -0700

    working on jquery results

--- scheduler/course_scheduler/views.py
+++ scheduler/course_scheduler/views.py
@@ -153,38 +153,38 @@ def return_test(request):
         return render(request, 'search_result.html', {'classes' : toSend});
     return None;
 
-def new_search(request):
-    #logging.debug('GOT A REQUEST')
-    toSend = {}
-    if request.method == 'GET':
-        #logging.debug('IT IS A GET')
-        #criterion = request.GET.get('Search', None)
-        #TODO better regexes
-        #patt = re.compile('(\w\w\w\w ((\w\w\w)|(\w\w\w\w)))|(\w\w\w\w\w\w\w)')
-##       patt = re.compile('(\w\w\w\w( )*(\d+|(\d+w)))')
-        criterion = request.GET['criterion']
-##        if patt.match(criterion):
-##            str = string.replace(criterion, ' ', '')
-##            arr = [None]*2
-##            arr[0] = str[0:3]
-##            arr[1] = str[4:]
-##            #arr = criterion.split(' ')
-##            classes = Instructs.objects.filter(meeting__meeting_class__dept__icontains=arr[0], meeting__meeting_class__class_number__icontains=arr[1])
-##        else:
-        classes = Instructs.objects.filter(Q(meeting__meeting_class__classname__icontains=criterion) | Q(meeting__meeting_class__dept__icontains=criterion) | Q(meeting__meeting_class__class_number__icontains=criterion))
-
-        for c in classes:
-            toSend[c] = True
-##            if Enrollment.objects.filter(student_id=id, event_id=c.meeting.id).exists():
-##                toSend[c] = True
-##            else:
-##                toSend[c] = False
-    else:
-        return None
-    
-    response = render(request, 'search_result.html', {'classes' : toSend})
-    #logging.debug('RETURNING')
-    return response
+##def new_search(request):
+##    #logging.debug('GOT A REQUEST')
+##    toSend = {}
+##    if request.method == 'GET':
+##        #logging.debug('IT IS A GET')
+##        #criterion = request.GET.get('Search', None)
+##        #TODO better regexes
+##        #patt = re.compile('(\w\w\w\w ((\w\w\w)|(\w\w\w\w)))|(\w\w\w\w\w\w\w)')
+####       patt = re.compile('(\w\w\w\w( )*(\d+|(\d+w)))')
+##        criterion = request.GET['criterion']
+####        if patt.match(criterion):
+####            str = string.replace(criterion, ' ', '')
+####            arr = [None]*2
+####            arr[0] = str[0:3]
+####            arr[1] = str[4:]
+####            #arr = criterion.split(' ')
+####            classes = Instructs.objects.filter(meeting__meeting_class__dept__icontains=arr[0], meeting__meeting_class__class_number__icontains=arr[1])
+####        else:
+##        classes = Instructs.objects.filter(Q(meeting__meeting_class__classname__icontains=criterion) | Q(meeting__meeting_class__dept__icontains=criterion) | Q(meeting__meeting_class__class_number__icontains=criterion))
+##
+##        for c in classes:
+##            toSend[c] = True
+####            if Enrollment.objects.filter(student_id=id, event_id=c.meeting.id).exists():
+####                toSend[c] = True
+####            else:
+####                toSend[c] = False
+##    else:
+##        return None
+##    
+##    response = render(request, 'search_result.html', {'classes' : toSend})
+##    #logging.debug('RETURNING')
+##    return response
 
 #   The info view is called whenever
 #   the user clicks on a meeting-time's
