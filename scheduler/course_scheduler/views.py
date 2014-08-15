84d74fe8b3797f664703c8a8a1a91fd2a3c6cb12
Stuart Long <stuart.richard.long@gmail.com>
2014-08-14 23:51:53 -0700

    working on add button

--- scheduler/course_scheduler/views.py
+++ scheduler/course_scheduler/views.py
@@ -176,7 +176,6 @@ def new_search(request):
                                             | Q(instructor__name__icontains=criterion))
 
         for c in classes:
-            toSend[c] = False
             if Enrollment.objects.filter(student_id=id, event_id=c.meeting.id).exists():
                 toSend[c] = True
             else:
