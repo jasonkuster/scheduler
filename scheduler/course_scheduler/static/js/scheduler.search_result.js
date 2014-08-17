a80059671bf6d6dd3a2469eaf01dbbc7d20fa9af
Stuart Long <stuart.richard.long@gmail.com>
2014-08-17 15:50:04 -0700

    externalizeing js

---
+++ scheduler/course_scheduler/static/js/scheduler.search_result.js
@@ -0,0 +1,53 @@
+function Add(eventID, studentID)
+{
+	$.post("/scheduler/add_course/", { 'eventID' : eventID, 'studentID' : studentID }, AddSuccess);	
+}
+
+function AddSuccess(data, textState, jqXHR)
+{
+	setRemoveButton(data.eventID, data.studentID);
+}
+
+function Remove(eventID, studentID)
+{
+	$.post("/scheduler/remove_course/", { 'eventID' : eventID, 'studentID' : studentID }, RemoveSuccess);	
+}
+
+function RemoveSuccess(data, textState, jqXHR)
+{
+	setAddButton(data.eventID, data.studentID);
+}
+
+function setRemoveButton(eventID, studentID)
+{
+	var eventIDWithoutQuotes = eventID.substring(1, eventID.length - 1);
+	$("#button-".concat(eventID)).html("<button class='btn btn-danger btn-xs' onclick='Remove(&quot;"+ eventID +"&quot;,&quot;"+ studentID+"&quot;)'>Remove</button>");
+}
+
+function setAddButton(eventID, studentID)
+{
+	var eventIDWithoutQuotes = eventID.substring(1, eventID.length - 1);
+	$("#button-".concat(eventID)).html("<button class='btn btn-success btn-xs' onclick='Add(&quot;"+ eventID +"&quot;,&quot;"+ studentID+"&quot;)'>Add</button>");
+}
+
+function ToggleInfo(eventID)
+{
+	if ($("#info-".concat(eventID)).is(":visible"))
+	{
+		ShowInfo(eventID);
+	}
+	else
+	{
+		HideInfo(eventID);
+	}
+}
+
+function ShowInfo(eventID)
+{
+	$("#info-".concat(eventID)).hide();
+}
+
+function HideInfo(eventID)
+{
+	$("#info-".concat(eventID)).hide();
+}
