04436e7f1a97193f932bddc59a4030231f2ef9c7
Stuart Long <stuart.richard.long@gmail.com>
2014-08-17 20:21:17 -0700

    externalized live search javascript

---
+++ scheduler/course_scheduler/static/js/scheduler.search.js
@@ -0,0 +1,17 @@
+function searchSuccess(data, textState, jqXHR)
+{
+	var html = new String(data);
+	if (html.substring(0, html.indexOf('</span>')).indexOf($("#course_search").val()) >= 0)
+	{
+		$("#search_result").html(data);
+	}
+}
+	
+function search()
+{	
+	if (new String($("#course_search").val()).length > 2)
+		{
+			$("#search_result").html('<div class="searching-panel">Searching . . .</div>');
+			$.get("/scheduler/search/", { 'criterion' : $("#course_search").val() }, searchSuccess, 'html');
+		}
+}
