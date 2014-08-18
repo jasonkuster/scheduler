f1ba29f5670fff6f953426e98e7ea3b2de9782ee
Stuart Long <stuart.richard.long@gmail.com>
2014-08-17 20:06:28 -0700

    doing some refactoring

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -21,7 +21,7 @@ urlpatterns = patterns('',
     url(r'^scheduler/about$', 'course_scheduler.views.old_views.about', name='about'),
     url(r'^scheduler/view/(\d{10})$', 'course_scheduler.views.old_views.shareview', name='view'),
     url(r'^scheduler/searchtest$', 'course_scheduler.views.old_views.searchtest', name='searchtest'),
-    url(r'^scheduler/search/$', 'course_scheduler.views.old_views.search', name='search'),
+    url(r'^scheduler/search/$', 'course_scheduler.views.search.search', name='search'),
     url(r'^scheduler/add_course/$', 'course_scheduler.views.old_views.add_course', name='add_course'),
     url(r'^scheduler/remove_course/$', 'course_scheduler.views.old_views.remove_course', name='remove_course'),
 )
