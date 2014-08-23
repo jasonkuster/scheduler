fe6f13c944a998f16b2bee0c3f4404cc11bd6e52
Stuart Long <stuart.richard.long@gmail.com>
2014-08-22 22:28:03 -0700

    fixing up issue caused by ASYN script importing

--- scheduler/course_scheduler/static/js/scheduler.csrf_util.js
+++ scheduler/course_scheduler/static/js/scheduler.csrf_util.js
@@ -2,11 +2,13 @@ function crsfSafeMethod(method) {
 		return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
-var csrftoken = $.cookie('csrftoken');
-$.ajaxSetup({
-	beforeSend: function(xhr, settings) {
-		if (!crsfSafeMethod(settings.type) && !this.crossDomain) {
-			xhr.setRequestHeader("X-CSRFToken", csrftoken);
+function initializeCSRF() {
+	var csrftoken = $.cookie('csrftoken');
+	$.ajaxSetup({
+		beforeSend: function(xhr, settings) {
+			if (!crsfSafeMethod(settings.type) && !this.crossDomain) {
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
 		}
-	}
-});
+	});
+}
