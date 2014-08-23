afbfced0987b2ec0107dcb857a2bf9d827fa294e
Stuart Long <stuart.richard.long@gmail.com>
2014-08-22 23:54:25 -0700

    doing some animations

--- scheduler/course_scheduler/static/js/scheduler.search.js
+++ scheduler/course_scheduler/static/js/scheduler.search.js
@@ -2,11 +2,20 @@ function searchSuccess(data, textState, jqXHR)
 {
 	if (data.substring(0, data.indexOf('</span>')).indexOf("'".concat($("#course_search").val()).concat("'")) >= 0)
 	{
+		$("#search_result").empty();
 		var html = $.parseHTML(data);
 		$(html).each(function(index, element) {
 			if ($(element).attr("id") === "result")
 			{
 				$("#search_result").append(element);
+				function() {
+				    var $lefty = $(element).next();
+				    $lefty.animate({
+				      left: parseInt($lefty.css('left'),10) == 0 ?
+				        -$lefty.outerWidth() :
+				        0
+				    });
+				  });
 			}
 		});
 		//$("#search_result").html(data);
